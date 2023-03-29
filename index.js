6c51d7e7baf3d2edf0c877b54d5c7d7e15f27a1a
Alexander Anthony <alexanderanthony@Alexanders-MacBook-Pro.local>
2023-03-29 15:28:30 -0300

    Image upload working

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors")
 const amigosPerdidosRouter = require("./routes/amigos");
 const s3Router = require("./routes/s3");
 
@@ -7,6 +8,8 @@ const PORT = 3000;
 const HOST_NAME = "localhost";
 
 const app = express();
+
+app.use(cors())
 app.use(express.static("client"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- routes/s3.js
+++ routes/s3.js
@@ -1,13 +1,16 @@
 const dotenv = require("dotenv");
 const aws = require("aws-sdk");
+// TODO import { S3Client, AbortMultipartUploadCommand } from "@aws-sdk/client-s3";
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/index.html
 const express = require("express");
+const crypto = require("crypto");
 
 dotenv.config();
 
 const s3Route = express.Router();
 
 const region = "sa-east-1";
-const bucketName = "amigosperdidos ";
+const bucketName = "amigosperdidos";
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
@@ -16,11 +19,13 @@ const s3 = new aws.S3({
   bucketName,
   accessKeyId,
   secretAccessKey,
+  signatureVersion: 'v4'
 });
 
 // Expires value is very high but who knows if someone on bad network uploading a picture...TODO revisit and decrease? not a huge security concern
 async function generatePresignedUrl() {
-  const imageName = "test123";
+  const rawBytes = await crypto.randomBytes(16)
+  const imageName = rawBytes.toString('hex');
   const params = {
     Bucket: bucketName,
     Key: imageName,
