f630420ea28bad217eaefa645dabcda2c22dbc42
Alexander Anthony <alexanderanthony@Alexanders-MacBook-Pro.local>
2023-06-08 19:23:09 -0400

    Schema simplified

--- models/index.js
+++ models/index.js
@@ -9,20 +9,22 @@ const userSchema = new Schema({
 
 export const User = model("User", userSchema);
 
+// will try to enforce but just the idea of the lifecycle here. lost and found are exclusive i think at least for UI purposes
+const permittedStatus = ["lost", "found", "sighted", "claimed", "reunited"]
+
 const amigoSchema = new Schema({
   species: String,
   last_seen_address: String,
   last_seen_date: Date,
   name: String,
+  status: String, // permittedStatus
   gender: String,
   description: String,
   message: String,
   photo_url: String,
   owner_id: String,
   stray: Boolean,
-  outdoor_amigo: Boolean,
-  missing: Boolean, // combination w found will make alerts etc
-  found: Boolean, // vice versa
+  owner_aware: Boolean, // can figure out later what the implications are but important to distinguish when the owner is *actually* taking care of the animal
 });
 
 export const Amigo = model("Amigo", amigoSchema);
