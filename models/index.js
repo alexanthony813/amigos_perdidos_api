f5814171df3cb0c5c20120e96218d8ce1f34055c
Alexander Anthony <alexanderanthony@Alexanders-MacBook-Pro.local>
2023-06-14 20:17:22 -0400

    Finalize status events

--- models/index.js
+++ models/index.js
@@ -9,22 +9,33 @@ const userSchema = new Schema({
 
 export const User = model("User", userSchema);
 
-// will try to enforce but just the idea of the lifecycle here. lost and found are exclusive i think at least for UI purposes
-const permittedStatus = ["lost", "found", "sighted", "claimed", "reunited"];
+// will try to enforce but just the idea of the lifecycle here. lost and found are exclusive i think at least for UI purposes.
+// sighted vs found difference is if the user has secured the dog, might not allow sighted without account
+// should automatically send message, next
+const messageSchema = new Schema({
+  sender_id: String,
+  receiver_id: String,
+  text: String,
+  time_sent: Date,
+  time_read: Date,
+});
+
+// "claimed" is tricky...want for dogs that are not identified but found
+const permittedStatus = ["lost", "sighted", "found", "reunited"];
 
 const statusEventSchema = new Schema({
   amigoId: String,
   status: String,
   time: Date,
-  details: String,
+  details: Object,
+  photo_url: String,
 });
 
 export const StatusEvent = model("StatusEvent", statusEventSchema);
 
 const amigoSchema = new Schema({
   species: String,
-  last_seen_address: String,
-  last_seen_date: Date,
+  last_seen_location: String,
   name: String,
   status: String, // permittedStatus
   sex: String,
@@ -34,7 +45,8 @@ const amigoSchema = new Schema({
   owner_id: String,
   stray: Boolean,
   owner_aware: Boolean, // can figure out later what the implications are but important to distinguish when the owner is *actually* taking care of the animal
-  last_status_event: String, // json object of statusEventSchema
+  last_status_event: Object, 
+  last_updated_at: Date,
 });
 
 export const Amigo = model("Amigo", amigoSchema);
