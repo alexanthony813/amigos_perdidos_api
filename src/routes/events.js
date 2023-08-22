ee0da78abadda72e1f0694408f7bf91d5c580c5d
Alexander Anthony <alexanderanthony@Alexanders-MacBook-Pro.local>
2023-08-22 18:42:07 -0400

    Refactor events

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,6 +1,11 @@
-import { StatusEvent, Quiltro, User } from "../models/index.js";
+import {
+  StatusEvent,
+  Quiltro,
+  User,
+  statusEventStatuses,
+} from "../models/index.js";
 import express from "express";
-import { twilioClient, twilioPhoneNUmber } from "../index.js"
+import { twilioClient, twilioPhoneNumber } from "../index.js";
 
 const eventsRouter = express.Router();
 
@@ -32,41 +37,50 @@ eventsRouter.post("/quiltros/:quiltroId/event", async (req, res) => {
     }
     if (newStatusEventJson.quiltroId !== quiltroId) {
       return res
-        .status(404)
+        .status(400)
         .send("quiltroId in body needs to match same in route");
+    }
+    if (statusEventStatuses.indexOf(newStatusEventJson.status) === -1) {
+      return res.status(400).send("need to use a valid status");
     }
     const quiltro = await Quiltro.findOne({ quiltroId });
     if (!quiltro) {
       return res.status(404).send("quiltro not found");
     }
     const now = new Date();
-    newStatusEventJson.time = now; // TODO figure out how to remove this for new records
+    newStatusEventJson.time = now;
     const newStatusEvent = new StatusEvent(newStatusEventJson);
     await newStatusEvent.save();
-    quiltro.lastStatusEvent = newStatusEvent;
+    if (newStatusEvent.status === "problem_reported") {
+      quiltro.lastReportedProblem = newStatusEvent;
+    }
     quiltro.lastUpdatedAt = now;
     await quiltro.save();
     const body = `Se informó un problema con ${quiltro.name}! \n Foto aquí: ${newStatusEvent.photoUrl} \n Mensaje del denunciante:${newStatusEvent.details.body}`;
     const { uid } = quiltro;
     const user = await User.findOne({ uid });
     const { phoneNumber } = user;
-    twilioClient.messages
-      .create({
-        body,
-        from: `whatsapp:${twilioPhoneNUmber}`,
-        to: `whatsapp:${phoneNumber}`,
-      })
-      .then((value) => {
-        return res.status(201).json(newStatusEvent);
-      })
-      .catch((error) => {
-        console.error(error);
-        return res.status(500).json(error);
-      });
+
+    if (newStatusEvent.status === "problem_reported") {
+      twilioClient.messages
+        .create({
+          body,
+          from: `whatsapp:${twilioPhoneNumber}`,
+          to: `whatsapp:${phoneNumber}`,
+        })
+        .then((value) => {
+          return res.status(201).json(newStatusEvent);
+        })
+        .catch((error) => {
+          console.error(error);
+          return res.status(500).json(error);
+        });
+    } else {
+      return res.status(201).json(newStatusEvent);
+    }
   } catch (err) {
     return res.status(500).json(err);
   }
 });
-
 
 export default eventsRouter;
